9416dec36472c31efa6f35b3472fe13752be9409
Cavey <Cavey@DESKTOP-CAVEYNE>
2025-09-23 00:23:24 +0800

    加入專案檔案。

---
+++ ToTraditionalChinese/ToTraditionalChinese.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using TaiwuModdingLib.Core.Plugin;
+using TMPro;
+using UnityEngine.UI;
+
+namespace ToTraditionalChinese
+{
+    [PluginConfig("ToTraditionalChinese","CaveyCoding","1.0.0")]
+    public class ToTraditionalChinese : TaiwuRemakePlugin
+    {
+        Harmony harmony;
+
+        public override void Dispose()
+        {
+            harmony?.UnpatchSelf();
+        }
+
+        public override void Initialize()
+        {
+            harmony = Harmony.CreateAndPatchAll(typeof(ToTraditionalChinese));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(TMP_Text), "text", MethodType.Setter)]
+        public static void TMPPrefix(Text __instance, ref string value)
+        {
+            value = Util.ToTraditionalChinese(value ?? "");
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(TMP_Text), "text", MethodType.Getter)]
+        public static void TMPPostfix(ref string __result)
+        {
+            __result = Util.ToTraditionalChinese(__result);
+        }
+    }
+}
---
+++ ToTraditionalChinese/Util.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using OpenCCNET;
+
+namespace ToTraditionalChinese
+{
+    public static class Util
+    {
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int LCMapString(int Locale, int dwMapFlags, string lpSrcStr, int cchSrc, [Out] string lpDestStr, int cchDest);
+        private const int LOCALE_SYSTEM_DEFAULT = 2048;
+        private const int LCMAP_SIMPLIFIED_CHINESE = 33554432;
+        private const int LCMAP_TRADITIONAL_CHINESE = 67108864;
+        private static bool ContainsChinese(this string str)
+        {
+            return Regex.IsMatch(str, "[\\u4e00-\\u9fa5]");
+        }
+        public static string ToTraditionalChinese(string input)
+        {
+            bool flag = input.ContainsChinese();
+            string result;
+            if (flag) {
+                string text = new string(' ', input.Length);
+                int num = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_TRADITIONAL_CHINESE, input, input.Length, text, input.Length);
+                result = text;
+            }
+            else
+                result = input;
+            return result;
+        }
+        public static string ToTraditionalChineseOpenCC(string input)
+        {
+            ZhConverter.Initialize();
+            return input.ToHantFromHans();
+        }
+    }
+}
