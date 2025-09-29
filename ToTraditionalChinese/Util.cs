554c0db2dab25e1f2755fc2c6d54bc9ef7f796d4
Cavey <m.123051427@gmail.com>
2025-09-29 20:36:24 +0800

    style: Format Util.cs code
    1. The constant 'LCMAP_SIMPLIFIED_CHINESE' has been commented out.
    2. The result variable of 'LCMapString' use discard variable(_).
    
    These change does not impact the active text conversion functionality.

--- ToTraditionalChinese/Util.cs
+++ ToTraditionalChinese/Util.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
-using OpenCCNET;
 
 namespace ToTraditionalChinese
 {
@@ -9,7 +8,7 @@ namespace ToTraditionalChinese
         [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern int LCMapString(int Locale, int dwMapFlags, string lpSrcStr, int cchSrc, [Out] string lpDestStr, int cchDest);
         private const int LOCALE_SYSTEM_DEFAULT = 2048;
-        private const int LCMAP_SIMPLIFIED_CHINESE = 33554432;
+        // private const int LCMAP_SIMPLIFIED_CHINESE = 33554432;
         private const int LCMAP_TRADITIONAL_CHINESE = 67108864;
         private static bool ContainsChinese(this string str)
         {
@@ -19,19 +18,15 @@ namespace ToTraditionalChinese
         {
             bool flag = input.ContainsChinese();
             string result;
-            if (flag) {
+            if (flag)
+            {
                 string text = new string(' ', input.Length);
-                int num = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_TRADITIONAL_CHINESE, input, input.Length, text, input.Length);
+                _ = LCMapString(LOCALE_SYSTEM_DEFAULT, LCMAP_TRADITIONAL_CHINESE, input, input.Length, text, input.Length);
                 result = text;
             }
             else
                 result = input;
             return result;
-        }
-        public static string ToTraditionalChineseOpenCC(string input)
-        {
-            ZhConverter.Initialize();
-            return input.ToHantFromHans();
         }
     }
 }
